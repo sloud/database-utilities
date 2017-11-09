70a094b2d30784fdbe0bff5c30478ce6a138d975
Nicklas Reincke <reincke@pier2port.de>
2017-11-09 17:57:24 +0100

    Changed module from `PrivateModule` to `AbstractModule`

--- src/main/java/com/reynke/sloud/databaseutilities/dependencyinjection/DatabaseUtilitiesModule.java
+++ src/main/java/com/reynke/sloud/databaseutilities/dependencyinjection/DatabaseUtilitiesModule.java
@@ -1,13 +1,13 @@
 package com.reynke.sloud.databaseutilities.dependencyinjection;
 
-import com.google.inject.PrivateModule;
+import com.google.inject.AbstractModule;
 import com.reynke.sloud.databaseutilities.configuration.IDatabaseConfiguration;
 import com.reynke.sloud.databaseutilities.database.IDatabase;
 
 /**
  * @author Nicklas Reincke (contact@reynke.com)
  */
-public class DatabaseUtilitiesModule extends PrivateModule {
+public class DatabaseUtilitiesModule extends AbstractModule {
 
     private final IDatabaseConfiguration databaseConfiguration;
 
@@ -17,11 +17,7 @@ public class DatabaseUtilitiesModule extends PrivateModule {
 
     @Override
     protected void configure() {
-
         bind(IDatabaseConfiguration.class).toInstance(databaseConfiguration);
         bind(IDatabase.class).to(databaseConfiguration.getDatabaseType().getDatabaseClass());
-
-        expose(IDatabaseConfiguration.class);
-        expose(IDatabase.class);
     }
 }
